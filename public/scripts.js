6fb678d63f1664b90eb93723ac68812b4f681354
@wenxihuw <wenxihuw@gmail.com>
2023-04-27 18:22:04 -0400

    Edit functions

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,34 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function showHideShots() {
+    let check = document.getElementById('opponent');
+    // let radiorps = document.getElementsByClassName('rps');
+    // let radiorpsls = document.getElementsByClassName('rpsls');
+
+    if (check.checked == true) {
+        $('.shots').show()
+    } else {
+        $('.shots').hide()
+    }
+}
+
+function startOver() {
+    document.getElementById('userinput').reset();
+    showHideShots();
+}
+
+async function playGame() {
+
+    let game = $('input[type=radio][name=game]:checked').val();
+    let shot = $('input[type=radio][name=shot]:checked').val();
+
+    let baseurl = window.location.href + 'app/'
+    console.log(baseurl)
+
+    let url = baseurl + game + '/play/'
+
+    let response = await fetch(url)
+    let result = await response.json()
+}
