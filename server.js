3b17fa12671cab2b76b7b47c76a2852427762d55
@wenxihuw <wenxihuw@gmail.com>
2023-05-04 17:13:08 -0400

    Link rpsls.js

--- server.js
+++ server.js
@@ -103,3 +103,28 @@ process.on('SIGINT', () => {
     })
 })
 
+import { rps, rpsls } from "./lib/rpsls.js";
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get("/app", (req, res) => {
+    res.status(200).send("200 OK");});
+app.get("/app/rps", (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));});
+app.get("/app/rpsls", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));});
+app.get("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));});
+app.get("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));});
+app.post("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));});
+app.post("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));});
+app.get("/app/rps/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));});
+app.get("/app/rpsls/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));});
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");});
